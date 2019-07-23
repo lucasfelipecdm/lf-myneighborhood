859121e7daded81fccfd44775cdfcad35d91db80
Lucas Felipe Costa da Mata <lucasfelipecdm@gmail.com>
2019-07-22 23:43:47 -0300

    feat: integrate with google maps API

--- src/App.js
+++ src/App.js
@@ -2,25 +2,48 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+
+  componentDidMount(){
+    this.renderMap()
+  }
+
+  renderMap = ()=> {
+    loadScripts("https://maps.googleapis.com/maps/api/js?key=AIzaSyBVGaI4iQj53OoM6-gWYfSKJgWtTEJUqq4&callback=initMap")
+    window.initMap = this.initMap 
+  }
+
+  initMap = ()=> {
+    const map = new window.google.maps.Map(document.getElementById('map'), {
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 8
+    });
+  }
+
+  render() {
+    return (
+      <main>
+        <div id="map"></div>
+      </main>
+    )
+  }
+}
+
+
+
+/* 
+  <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap"
+    async defer></script>
+*/
+
+function loadScripts(url){
+  var index = window.document.getElementsByTagName("script")[0]
+  var script = window.document.createElement("script")
+  script.src = url
+  script.defer = true
+  script.async = true
+  index.parentNode.insertBefore(script, index)
 }
 
 export default App;
