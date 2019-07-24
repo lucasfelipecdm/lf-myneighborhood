96cd21b2c0318f802681b96a4a0c61b33d80557b
Lucas Felipe Costa da Mata <lucasfelipecdm@gmail.com>
2019-07-24 03:47:28 -0300

    feat: set markers like the filter search

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ class App extends React.Component {
 
   state = {
     venues: [],
+    firstRender: true,
+    map: '',
+    markers: '',
     searchVenues: (query) => {
       const endPoint = "https://api.foursquare.com/v2/venues/explore?"
       const parameters = {
@@ -17,10 +20,19 @@ class App extends React.Component {
         v: "20190723"
       }
       axios.get(endPoint + new URLSearchParams(parameters))
-        .then(response => {
+        .then(response => {          
           this.setState({
             venues: response.data.response.groups[0].items
-          }, this.renderMap())
+          }, () => {
+            if(this.state.firstRender){
+              this.renderMap();
+              this.setState({
+                firstRender: false
+              })
+            } else {
+              this.placeMarkers();
+            }
+          })
         })
         .catch(error => {
           console.log(`Error: ${error}`)
@@ -43,21 +55,37 @@ class App extends React.Component {
       center: { lat: -22.2292377, lng: -45.9382979},
       zoom: 14
     });
+    this.setState({
+      map: map,
+    }, () => this.placeMarkers())
+  }
 
-    var infowindow = new window.google.maps.InfoWindow();
+  placeMarkers = () => {
+    if(this.state.markers) {
+      this.state.markers.forEach(marker => {
+        marker.setMap(null);
+      })
+    }
 
-    this.state.venues.map(venue => {
+    
+
+    const markers = this.state.venues.map(venue => {
       var contentString = `<h1>${venue.venue.name}</h1>`;
+      var infowindow = new window.google.maps.InfoWindow();
       var marker = new window.google.maps.Marker({
         position: { lat: venue.venue.location.lat, lng: venue.venue.location.lng },
-        map: map,
         title: venue.venue.name
       });
       marker.addListener('click', function () {
         infowindow.setContent(contentString);
-        infowindow.open(map, marker);
+        infowindow.open(this.state.map, marker);
       });
-      return 0;
+      marker.setMap(this.state.map);
+      console.log(marker);
+      return marker;
+    })
+    this.setState({
+      markers: markers,
     })
   }
 
@@ -65,7 +93,7 @@ class App extends React.Component {
     return (
       <main>
         <div id="map"></div>
-        <SideBar searchVenues={this.state.searchVenues}></SideBar>
+        <SideBar searchVenues={this.state.searchVenues} venues={this.state.venues}></SideBar>
       </main>
     )
   }
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-function SideBar (props) {
+function SideBar(props) {
     const btnHamburguer = document.getElementById('hamburger-button');
-    const sidebar =  document.getElementById('sidebar');
+    const sidebar = document.getElementById('sidebar');
     const toggleClass = () => {
         sidebar.classList.contains('open') ? sidebar.classList.remove('open') : sidebar.classList.add('open');
         btnHamburguer.classList.contains('open') ? btnHamburguer.classList.remove('open') : btnHamburguer.classList.add('open');
@@ -10,15 +10,26 @@ function SideBar (props) {
 
     return (<menu>
         <button onClick={e => toggleClass()} id="hamburger-button">
-                <div className="bar-hamburger-button1"></div>
-                <div className="bar-hamburger-button2"></div>
-                <div className="bar-hamburger-button3"></div>
+            <div className="bar-hamburger-button1"></div>
+            <div className="bar-hamburger-button2"></div>
+            <div className="bar-hamburger-button3"></div>
         </button>
         <div className="menu" id="sidebar">
-            <form>
-                <label forhtml="search">Search:</label>
-                <input id="search" onChange={(e) => props.searchVenues(e.target.value)}></input>
-            </form>
+            <div id="search-div">
+                <form>
+                    <label forhtml="search">Search:</label>
+                    <input type="search" id="search" onChange={(e) => props.searchVenues(e.target.value)}></input>
+                </form>
+            </div>
+            <div id="markers-list-div">
+                <ol>
+                    {props.venues.map(venue => (
+                        <li key={venue.venue.id}>
+                            {venue.venue.name}
+                        </li>
+                    ))}
+                </ol>
+            </div>
         </div>
     </menu>);
 }
